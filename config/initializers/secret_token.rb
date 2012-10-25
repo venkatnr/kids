f28c20c7236a5f917e645476d2329d502d32a2a2
liplw016 <liplw016@liplw016-Vostro-1550.(none)>
2012-10-25 10:03:25 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '3b5eec16f205e55d7d50e70af7a2caa47a5fc41a8b67c7a2673ff1ee6e5c74a00c01bab917456468e67f9cf043719ea93b0d77ba781b596cb55662ea635b3c34'
